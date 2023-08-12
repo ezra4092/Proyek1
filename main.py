202c2e38689b79d0cc53696c70e390a66bfcde94
ezra4092 <141468634+ezra4092@users.noreply.github.com>
2023-08-12 10:50:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL" : "Tanggapan terhadap lelucon",
-            "SHEESH" : "Sedikit ketidaksetujuan",
-            "CREEPY" : "Menakutkan, tidak menyenangkan",
-            "AGGRO" : "Untuk menjadi agresif/marah"
-            }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-    # Apa yang harus kita lakukan jika kata itu ditemukan?
-else:
-    print("Kata itu tidak ada di dalam kamus")
-    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+import random
+keyword =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+password = ""
+jumlah = int(input("Mau buat berapa password yang mau dihasilkan? "))
+for i in range (jumlah) :
+    password += random.choice(keyword)
+print (password)
